5fd581624b5867b210c1e875f0a5391f613cb952
Jaime Moscoso <jaime.moscoso@gmail.com>
2022-06-24 22:36:17 -0400

    Add UpdateMaxBillsThreshold project

---
+++ UpdateMaxBillsThreshold/UpdateMaxBillsThreshold/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateMaxBillsThreshold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey("SOFTWARE\\NCR\\Advance NDC\\CurrencyTable\\RejectBin\\");
+            key.SetValue("MaxBillsThreshold", "200");
+            key.Close();
+        }
+    }
+}
