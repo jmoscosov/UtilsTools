0c13eaba4944ce846d6c4d8c807a57447d74eb87
Jaime Moscoso <jaime.moscoso@gmail.com>
2022-06-24 22:40:28 -0400

    Add FixM19 Project

---
+++ FixM19/FixM19/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixM19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Win32.RegistryKey key,key2;
+            key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\ATM\\DEVINFO\\CDM");
+            key2 = Microsoft.Win32.Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\NCR\\Advance NDC\\supervisor");
+
+            try
+            {
+                key.SetValue("ShutterLockCheckInPresent", "0", Microsoft.Win32.RegistryValueKind.DWord);
+                key2.SetValue("EnableAERFlagDuringSimSupervisor", "0", Microsoft.Win32.RegistryValueKind.DWord);
+                key2.SetValue("SuppressSupervisorAERMessages", "2", Microsoft.Win32.RegistryValueKind.DWord);
+
+
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+
+                Console.WriteLine("UnauthorizedAccessException: {0}", ex.Source);
+            }
+            catch (System.Security.SecurityException ex)
+            {
+                Console.WriteLine("SecurityException source: {0}", ex.Source);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception source: {0}", ex.Source);
+            }
+            
+            key.Close();
+        }
+    }
+}
