6e3032bffed384ffad479b54ba471e755a7cd55c
Jaime Moscoso <jaime.moscoso@gmail.com>
2023-01-04 11:17:22 -0300

    add parameters for install and uninstall and Logs

---
+++ FixM19/FixM19/LoggerClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace FixM19
+{
+    internal static class LoggerClass
+    {
+        private const string LOG_FILENAME = @"C:\NCRLogs\FixM19.LOG";
+
+        internal static void Log(string text)
+        {
+                File.AppendAllText(LOG_FILENAME, $"{DateTime.Now} {text}\r\n");
+        }
+
+        internal static void Log(object obj)
+        {
+            Log(obj.ToString());
+        }
+    }
+}
--- FixM19/FixM19/Program.cs
+++ FixM19/FixM19/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace FixM19
 {
     class Program
@@ -13,7 +14,7 @@ namespace FixM19
             Microsoft.Win32.RegistryKey key, key2;
             key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\ATM\\DEVINFO\\CDM");
             key2 = Microsoft.Win32.Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\NCR\\Advance NDC\\supervisor");
-
+            LoggerClass.Log($"Fix M19 Running");
             if (args[0] == "/i")
             {
 
@@ -21,9 +22,9 @@ namespace FixM19
                 {
                     Console.WriteLine("Install Command Fix M19");
                     key.SetValue("ShutterLockCheckInPresent", "0", Microsoft.Win32.RegistryValueKind.DWord);
-                    key2.SetValue("EnableAERFlagDuringSimSupervisor", "0", Microsoft.Win32.RegistryValueKind.DWord);
+                    key2.SetValue("EnableAERFlagDuringSimSupervisor", "1", Microsoft.Win32.RegistryValueKind.DWord);
                     key2.SetValue("SuppressSupervisorAERMessages", "2", Microsoft.Win32.RegistryValueKind.DWord);
-
+                    LoggerClass.Log($"Fix M19 Installation process completed");
 
                 }
                 catch (UnauthorizedAccessException ex)
@@ -47,10 +48,10 @@ namespace FixM19
                 try
                 {
                     Console.WriteLine("Uninstall Command Fix M19");
-                    key.SetValue("ShutterLockCheckInPresent", "0", Microsoft.Win32.RegistryValueKind.DWord);
+                    key.SetValue("ShutterLockCheckInPresent", "1", Microsoft.Win32.RegistryValueKind.DWord);
                     key2.SetValue("EnableAERFlagDuringSimSupervisor", "0", Microsoft.Win32.RegistryValueKind.DWord);
-                    key2.SetValue("SuppressSupervisorAERMessages", "2", Microsoft.Win32.RegistryValueKind.DWord);
-
+                    key2.SetValue("SuppressSupervisorAERMessages", "0", Microsoft.Win32.RegistryValueKind.DWord);
+                    LoggerClass.Log($"Fix M19 Uninstallation process completed");
 
                 }
                 catch (UnauthorizedAccessException ex)
